9d922d38aa0fb5a55b4b059c64f5aa0e231af2f6
Nathanaël Renaud <perso@renaudna.fr>
2024-06-25 00:19:50 +0200

    Add response to the message sent.

--- js/content.js
+++ js/content.js
@@ -1,5 +1,9 @@
-chrome.runtime.onMessage.addListener((message) => {
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.type === "change-title") {
         document.title = document.title + " - Hacked !";
+        setTimeout(() => {
+            sendResponse(document.title + " - Hacked successfuly !");
+        }, 5000);
     }
+    return true;
 });
--- js/popup.js
+++ js/popup.js
@@ -2,7 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const button = document.getElementById("button-changeTitle");
     button.addEventListener("click", () => {
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-            chrome.tabs.sendMessage(tabs[0].id, { type: "change-title" });
+            button.setAttribute("disabled", "true");
+            chrome.tabs.sendMessage(tabs[0].id, { type: "change-title" }, function (response) {
+                button.removeAttribute("disabled");
+            });
         });
     });
 });
